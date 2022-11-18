079c757e524b5ca6787db5610f0eede9bc503d67
Arop <59123451+AropJoe@users.noreply.github.com>
2022-11-18 17:59:50 +0100

    use path in place of urls

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from tracking.views import (
     dashboard,
@@ -11,11 +11,11 @@ from tracking.views import (
 )
 
 urlpatterns = [
-    url(r'^$', dashboard, name='tracking-dashboard'),
-    url(r'^visitors/(?P<user_id>.*)/page/$', visitor_page_detail, name='tracking-visitor-page-detail'),
-    url(r'^visitors/(?P<user_id>.*)/pageview/(?P<pageview_id>.*)/$', visitor_pageview_detail, name='tracking-pageview-detail'),
-    url(r'^visitors/(?P<user_id>.*)/$', visitor_overview, name='tracking-visitor-overview'),
-    url(r'^visits/(?P<visit_id>.*)/$', visitor_detail, name='tracking-visitor-detail'),
-    url(r'^pages/$', page_overview, name='tracking-page-overview'),
-    url(r'^page/$', page_detail, name='tracking-page-detail'),
+    path('', dashboard, name='tracking-dashboard'),
+    path('visitors/<user_id>)/page/', visitor_page_detail, name='tracking-visitor-page-detail'),
+    path('visitors/<user_id>)/pageview/<pageview_id>/', visitor_pageview_detail, name='tracking-pageview-detail'),
+    path('visitors/<user_id>)/', visitor_overview, name='tracking-visitor-overview'),
+    path('visits/<visit_id>/', visitor_detail, name='tracking-visitor-detail'),
+    path('pages/', page_overview, name='tracking-page-overview'),
+    path('page/', page_detail, name='tracking-page-detail'),
 ]
